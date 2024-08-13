f6dff7b3e977cb4f69e2b299c8ebc4162a36b2ac
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2024-08-13 23:20:48 +0200

    Adding changes

--- fibonacci2.php
+++ fibonacci2.php
@@ -1,31 +1,64 @@
 <?php
-/*multiply each number by 2 and print it from biggest to smallest*/ 
 
 function fibonacci($n) {
-    $fibonacciSequence = array();
+    if ($n <= 0) {
+        return "Please enter a positive integer greater than zero.";
+    }
 
-    if ($n >= 1) {
-        $fibonacciSequence[] = 0;
-    }
-    if ($n >= 2) {
-        $fibonacciSequence[] = 1;
-    }
+    $fibonacciSequence = [0, 1];
 
     for ($i = 2; $i < $n; $i++) {
-        $fibonacciSequence[] = $fibonacciSequence[$i - 1] + $fibonacciSequence[$i - 2];
+        $fibonacciSequence[$i] = $fibonacciSequence[$i - 1] + $fibonacciSequence[$i - 2];
+    }
+
+    if ($n == 1) {
+        $fibonacciSequence = [0];
     }
 
     return $fibonacciSequence;
 }
 
+function multiplyByTwo($sequence) {
+    $multipliedSequence = [];
+    for ($i = 0; $i < count($sequence); $i++) {
+        $multipliedSequence[$i] = $sequence[$i] * 2;
+    }
+    return $multipliedSequence;
+}
+
+function sortDescending(&$sequence) {
+    $length = count($sequence);
+    for ($i = 0; $i < $length - 1; $i++) {
+        for ($j = 0; $j < $length - $i - 1; $j++) {
+            if ($sequence[$j] < $sequence[$j + 1]) {
+                $temp = $sequence[$j];
+                $sequence[$j] = $sequence[$j + 1];
+                $sequence[$j + 1] = $temp;
+            }
+        }
+    }
+}
+
+function arrayToString($sequence) {
+    $string = "";
+    for ($i = 0; $i < count($sequence); $i++) {
+        $string .= $sequence[$i];
+        if ($i < count($sequence) - 1) {
+            $string .= ", ";
+        }
+    }
+    return $string;
+}
+
 $numberOfTerms = 10;
 $fibonacciSequence = fibonacci($numberOfTerms);
 
-$fibonacciMultiplied = array_map(function ($value) {
-    return $value * 2;
-}, $fibonacciSequence);
+if (is_array($fibonacciSequence)) {
+    $fibonacciMultiplied = multiplyByTwo($fibonacciSequence);
+    sortDescending($fibonacciMultiplied);
+    echo arrayToString($fibonacciMultiplied);
+} else {
+    echo $fibonacciSequence;
+}
 
-rsort($fibonacciMultiplied);
-
-implode(', ', $fibonacciMultiplied);
 ?>
