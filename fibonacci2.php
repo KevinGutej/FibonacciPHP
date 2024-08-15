f2d2fd72d9b03970c7ad3d53c26929d429bdcf3b
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2024-08-15 22:20:34 +0200

    Improved efficiency and readability, added new functions

--- fibonacci2.php
+++ fibonacci2.php
@@ -1,36 +1,33 @@
 <?php
 
 function fibonacci($n) {
-    if ($n <= 0) {
+    if (!is_int($n) || $n <= 0) {
         return "Please enter a positive integer greater than zero.";
     }
 
-    $fibonacciSequence = [0, 1];
-
-    for ($i = 2; $i < $n; $i++) {
-        $fibonacciSequence[$i] = $fibonacciSequence[$i - 1] + $fibonacciSequence[$i - 2];
+    if ($n === 1) {
+        return [0];
     }
 
-    if ($n == 1) {
-        $fibonacciSequence = [0];
+    $fibonacciSequence = [0, 1];
+    for ($i = 2; $i < $n; $i++) {
+        $fibonacciSequence[] = $fibonacciSequence[$i - 1] + $fibonacciSequence[$i - 2];
     }
 
     return $fibonacciSequence;
 }
 
-function multiplyByTwo($sequence) {
-    $multipliedSequence = [];
-    for ($i = 0; $i < count($sequence); $i++) {
-        $multipliedSequence[$i] = $sequence[$i] * 2;
-    }
-    return $multipliedSequence;
+function multiplyBy($sequence, $factor) {
+    return array_map(function($num) use ($factor) {
+        return $num * $factor;
+    }, $sequence);
 }
 
-function sortDescending(&$sequence) {
+function sortArray(&$sequence, $ascending = true) {
     $length = count($sequence);
     for ($i = 0; $i < $length - 1; $i++) {
         for ($j = 0; $j < $length - $i - 1; $j++) {
-            if ($sequence[$j] < $sequence[$j + 1]) {
+            if (($ascending && $sequence[$j] > $sequence[$j + 1]) || (!$ascending && $sequence[$j] < $sequence[$j + 1])) {
                 $temp = $sequence[$j];
                 $sequence[$j] = $sequence[$j + 1];
                 $sequence[$j + 1] = $temp;
@@ -40,23 +37,44 @@ function sortDescending(&$sequence) {
 }
 
 function arrayToString($sequence) {
-    $string = "";
-    for ($i = 0; $i < count($sequence); $i++) {
-        $string .= $sequence[$i];
-        if ($i < count($sequence) - 1) {
-            $string .= ", ";
-        }
-    }
-    return $string;
+    return implode(", ", $sequence);
+}
+
+function sumOfSequence($sequence) {
+    return array_sum($sequence);
+}
+
+function filterEven($sequence) {
+    return array_filter($sequence, function($num) {
+        return $num % 2 === 0;
+    });
+}
+
+function filterOdd($sequence) {
+    return array_filter($sequence, function($num) {
+        return $num % 2 !== 0;
+    });
 }
 
 $numberOfTerms = 10;
 $fibonacciSequence = fibonacci($numberOfTerms);
 
 if (is_array($fibonacciSequence)) {
-    $fibonacciMultiplied = multiplyByTwo($fibonacciSequence);
-    sortDescending($fibonacciMultiplied);
-    echo arrayToString($fibonacciMultiplied);
+    $fibonacciMultiplied = multiplyBy($fibonacciSequence, 2);
+    
+    sortArray($fibonacciMultiplied, false);
+    
+    $sequenceString = arrayToString($fibonacciMultiplied);
+    
+    $sequenceSum = sumOfSequence($fibonacciMultiplied);
+    
+    $evenNumbers = arrayToString(filterEven($fibonacciMultiplied));
+    $oddNumbers = arrayToString(filterOdd($fibonacciMultiplied));
+    
+    echo "Fibonacci Sequence: $sequenceString\n";
+    echo "Sum of Sequence: $sequenceSum\n";
+    echo "Even Numbers: $evenNumbers\n";
+    echo "Odd Numbers: $oddNumbers\n";
 } else {
     echo $fibonacciSequence;
 }
